b856600c1c71270cdeb92de25defb64009e28964
tokizg <145384085+tokizg@users.noreply.github.com>
2024-11-06 12:02:15 +0900

    第5回

--- Assets/CharaController.cs
+++ Assets/CharaController.cs
@@ -17,6 +17,9 @@ public class CharaController : MonoBehaviour
     private Vector3 moveDir = Vector3.zero; // 移動の方向を計算するための変数
     private Vector3 sprintDir = Vector3.zero; // ダッシュの方向を計算するための変数
 
+    [SerializeField]
+    private float rotationSpeed = 720.0f; // 回転の速さ
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,6 +43,16 @@ public class CharaController : MonoBehaviour
             direction += Physics.gravity * Time.fixedDeltaTime; // 地面に接地していない場合は、重力をかける
         }
 
+        if (moveDir.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir); // 向かせたい方向を計算する
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.fixedDeltaTime
+            ); // キャラクターの向きを徐々に変える
+        }
+
         cc.Move(direction); // キャラクターを移動させる
         anim.SetFloat("Speed", cc.velocity.magnitude); // アニメーションのパラメーター"Speed"に、現在のキャラクターの速度を渡す
     }
@@ -47,7 +60,7 @@ public class CharaController : MonoBehaviour
     public void Move(Vector2 direction)
     {
         moveDir = new Vector3(direction.x, 0, direction.y).normalized; // 方向を計算する
-        moveDir = transform.TransformDirection(moveDir); // キャラクターの向きに合わせて方向を変える
+        //moveDir = transform.TransformDirection(moveDir); // キャラクターの向きに合わせて方向を変える
         moveDir *= walkSpeed; // 速度をかける
     }
 
